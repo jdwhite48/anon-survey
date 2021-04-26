e30df5a7c889a8c1c2480db2fb1e2dd505aafad5
jdwhite88 <white570@purdue.edu>
2021-04-26 01:24:54 -0400

    Properly get generator pairs

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,36 @@ use users::RA::RegistrationAuthority;
 use tbn::{Group, Fr, Fq, G1, G2, pairing};
 use tbn::arith::U256;
 
-fn main() {
-    println!("Hello, world!");
-    
-    
+
+
+// Returns generators (g, g2) in (G1, G2)
+// Because G1 and G2 are additive cyclic groups of prime order by construction of BN curves
+// It is sufficient to randomly choose elements in G1 and G2 to get g and g2
+fn get_generator_pair() -> (G1, G2) {
     
     // Crytpographiclaly secure thread-local rng
     let rng = &mut rand::thread_rng();
+
+    // Generate random elements in G1 and G2
+    let (mut g, mut g2):(G1, G2) = (G1::random(rng), G2::random(rng));
+    // Ensure that g,g2 are both generators (i.e. non-zero in additive cyclic group of prime
+    // order)
+    while g.is_zero() {
+        g = G1::random(rng);
+    }
+    while g2.is_zero() {
+        g2 = G2::random(rng);
+    }
+
+    // Return generator pair
+    (g, g2)
+}
+
+fn main() {
     
-    // Construct "generators" g in G_1 and g_2 in G_2? 
-    // TODO: What are the actual generators of G_1 and G_2 ???
-    let (g, g2):(G1, G2) = (G1::one() * Fr::random(rng), G2::one() * Fr::random(rng));
+    // Crytpographiclaly secure thread-local rng
+    let rng = &mut rand::thread_rng();
+
     
     // TODO: Is this the right modulus / order of cyclic groups ???
     let q:U256 = Fq::modulus();
@@ -28,6 +47,7 @@ fn main() {
     println!();
 
     // TODO: Have all users run on separate threads
+    let (g, g2):(G1, G2) = get_generator_pair();
 
     // Instantiate new Registration Authority
     let ra:RegistrationAuthority = RegistrationAuthority::new(g, g2);
@@ -46,4 +66,19 @@ fn main() {
     // TODO: Figure out how to print something of type Gt
 //    println!("pair = {:?}", sa.vk.pk.0);
     println!();
+}
+
+
+// Fuzzy test for if we have a good generator for pairing-based crypto
+#[test]
+fn test_generators() {
+    let (g, g2):(G1, G2) = get_generator_pair();
+    // Try 5 different random values to see if assertion holds each time
+    // For random a and b, asserts that e(g^a, g_2^b) = e(g,g_2)^{ab} (RHS is generator for Gt)
+    let rng = &mut rand::thread_rng();
+    for _ in 0..5 {
+        let a = Fr::random(rng);
+        let b = Fr::random(rng);
+        assert!( pairing(g * a, g2 * b) == pairing(g, g2).pow(a * b) );
+    }
 }
